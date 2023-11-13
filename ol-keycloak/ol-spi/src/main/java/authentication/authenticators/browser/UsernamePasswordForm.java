141d0d44acb2c8dc476ce77c1626480cccdc0c4c
Collin Preston <collin_preston@yahoo.com>
2023-11-13 13:19:15 -0500

    Fix login URI for unlinked providers

--- ol-keycloak/ol-spi/src/main/java/authentication/authenticators/browser/UsernamePasswordForm.java
+++ ol-keycloak/ol-spi/src/main/java/authentication/authenticators/browser/UsernamePasswordForm.java
@@ -1,12 +1,12 @@
 package authentication.authenticators.browser;
 
+import jakarta.ws.rs.core.UriBuilder;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.forms.login.LoginFormsProvider;
 import org.keycloak.forms.login.freemarker.LoginFormsUtil;
 import org.keycloak.forms.login.freemarker.model.IdentityProviderBean;
-import org.keycloak.models.IdentityProviderModel;
-import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.RealmModel;
+import org.keycloak.models.*;
+import org.keycloak.services.resources.LoginActionsService;
 
 import java.net.URI;
 import java.util.List;
@@ -24,8 +24,19 @@ public class UsernamePasswordForm extends org.keycloak.authentication.authentica
             List<IdentityProviderModel> realmIdentityProvidersList = realm.getIdentityProvidersStream().collect(Collectors.toList());
             LoginFormsProvider form = context.form();
             realmIdentityProvidersList.removeAll(identityProvidersLinkedWithUser);
-
-            form.setAttribute("unlinkedProviders", new IdentityProviderBean(realm, session, realmIdentityProvidersList, URI.create("")));
+            String requestURI = session.getContext().getUri().getBaseUri().getPath();
+            UriBuilder uriBuilder = UriBuilder.fromUri(requestURI);
+            ClientModel client = session.getContext().getClient();
+            if (client != null) {
+                uriBuilder.queryParam(Constants.CLIENT_ID, client.getClientId());
+            }
+            if (context.getAuthenticationSession() != null) {
+                uriBuilder.queryParam(Constants.TAB_ID, context.getAuthenticationSession().getTabId());
+                String accessCode = context.generateAccessCode();
+                uriBuilder.queryParam(LoginActionsService.SESSION_CODE, accessCode);
+            }
+            URI baseUriWithCodeAndClientId = uriBuilder.build();
+            form.setAttribute("unlinkedProviders", new IdentityProviderBean(realm, session, realmIdentityProvidersList, baseUriWithCodeAndClientId));
         }
         super.authenticate(context);
     }
